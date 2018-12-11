24d18859588bca39e58d34023cb98a68a43f5478
godofsaw <44059221+godofsaw@users.noreply.github.com>
2018-12-11 19:50:40 +0200

    new

--- app/src/main/java/com/example/gos/chat/ChatMessage.java
+++ app/src/main/java/com/example/gos/chat/ChatMessage.java
@@ -11,6 +11,7 @@ public class ChatMessage {
     private String messageText;
     private String messageUser;
     private long messageTime;
+    private String dfdsfdf;
 
     public ChatMessage(String messageText, String messageUser) {
         this.messageText = messageText;
